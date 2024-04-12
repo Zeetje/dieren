457d73355a213789153871a8ac94cfe4ed0285ca
89329 <89329@glr.nl>
2024-04-12 11:07:32 +0200

    .

--- php/login.php
+++ php/login.php
@@ -1,24 +1,22 @@
 <?php
-include 'db.php';
+include 'config.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $sql = "SELECT id, username, password FROM users WHERE username='$username'";
+    $sql = "SELECT password FROM users WHERE username='$username'";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         $row = $result->fetch_assoc();
         if (password_verify($password, $row['password'])) {
-            echo "Inloggen succesvol!";
+            echo "U bent ingelogd!";
         } else {
-            echo "Verkeerd wachtwoord!";
+            echo "Onjuiste gebruikersnaam of wachtwoord!";
         }
     } else {
-        echo "Gebruiker niet gevonden!";
+        echo "Onjuiste gebruikersnaam of wachtwoord!";
     }
 }
-
-$conn->close();
 ?>
--- php/register.php
+++ php/register.php
@@ -1,18 +1,16 @@
 <?php
-include 'db.php';
+include 'config.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
     $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
-
+    
     if ($conn->query($sql) === TRUE) {
         echo "Registratie succesvol!";
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
 }
-
-$conn->close();
 ?>
